eae79895409fd5595eb9da21d82052f2f27706b5
noamoss <noam@hasadna.org.il>
2014-03-31 16:55:14 +0300

    Add OpenTrain

--- eKnightsData.js
+++ eKnightsData.js
@@ -1,169 +1,93 @@
-/***
- * @description 
- * @static
- * @public
- * @constant
- * @type Array
- */
-var eKnightsData =
-        [
-            {
-                "name": "כנסת פתוחה",
-                "permalink": "http://oknesset.org",
-                "about": "החשמביר הראשון והאמא של הסדנא.  משרת פעילים וחברי כנסת למעלה משלוש שנים",
-                "status": "פרח מן הקן",
-                "entry_level": "מתקדמים",
-                "issuesUrl": "https://api.github.com/repos/hasadna/Open-Knesset/issues",
-                "slug": "open-knesset",
-                "discussion_board": "https://groups.google.com/forum/#!forum/open-knesset",
-                "github_repo": "https://github.com/hasadna/Open-Knesset",
-                "leaders": [
-                    "ofri",
-                    "MeirKriheli",
-                    "amirnissim"
-                ]
-            },
-            {
-                "name": "קהילה פתוחה",
-                "permalink": "http://opencommunity.org.il",
-                "about": "הגברת רמת המעורבות של הציבור בקהילה על ידי שיתופו באופן שיטתי בתהליך קבלת ההחלטות, ומימוש, הלכה למעשה, את היותו הריבון של הקהילה.",
-                "status": "בקע מן הביצה",
-                "entry_level": "מתקדמים",
-                "discussion_board": "https://groups.google.com/forum/#!forum/omiflaga",
-                "issuesUrl": "https://api.github.com/repos/hasadna/OpenCommunity/issues",
-                "slug": "open-community",
-                "github_repo": "https://github.com/hasadna/OpenCommunity",
-                "leaders": [
-                    "BoazChen",
-                    "nonZero"
-                ]
-            },
-            {
-                "name": "התקציב המקומי הפתוח",
-                "about": "עידוד רשויות מקומיות להתנהל לפי עקרונות של שקיפות ושיתוף הציבור והגברת מעורבותם של התושבים בהתנהלות הממשל המקומי",
-                "status": "דגירה",
-                "entry_level": "גיבורי-על",
-                "github_repo": "https://github.com/hasadna/openmuni-budgets",
-                "issuesUrl": "https://api.github.com/repos/hasadna/openmuni-budgets/issues",
-                "slug": "openmuni-budgets",
-                "leaders": [
-                    "pwalsh",
-                    "idoivri",
-                    "ydaniv"
-                ],
-                "discussion_board": "https://groups.google.com/forum/#!forum/open-municipality"
-            },
-            {
-                "name": "פותחן התקציב",
-                "permalink": "http://compare.open-budget.org.il",
-                "about": "השוואת תקציב המדינה לשנת 2013/2014 לתקציב השנים הקודמות והצפת הסעיפים שגדלו במיוחד או קטנו (בוצע בשיתןף עם ׳כלכליסט׳",
-                "status": "פרח מן הקן",
-                "entry_level": "גיבורי-על",
-                "issuesUrl": "https://api.github.com/repos/hasadna/open-budget/issues",
-                "slug": "open-budget",
-                "github_repo": "https://github.com/hasadna/openmuni-budgets",
-                "leaders": [
-                    "akariv",
-                    "mushon"
-                ]
-            },
-            {
-                "name": "התקציב הפתוח",
-                "permalink": "http://budget.msh.gov.il",
-                "about": "הצגת תקציב המדינה, מאז 1992 ועד 2014, השאאה היסטורית חיפוש ועוד",
-                "status": "פרח מן הקן",
-                "entry_level": "גיבורי-על",
-                "github_repo": "https://github.com/hasadna/obudget",
-                "issuesUrl": "https://api.github.com/repos/hasadna/obudget/issues",
-                "slug": "obudget",
-                "leaders": [
-                    "akariv",
-                    "daonb"
-                ]
-            },
-            /*            {
-             "name": "מעקב טרכטנברג",
-             "permalink": "http://watch.gov.il",
-             "about": "מעקב אחר יישום המלצות וועדת טרכטנברג (הושק לרגל שנה למחאה החברתית, בשיתוף עם הממשלה וארגוני חברה אזרחית)",
-             "status": "פרח מן הקן",
-             "entry_level": "גיבורי-על",
-             "issuesUrl": "https://api.github.com/repos/hasadna/gov-watch/issues",
-             "slug": "gov-watch",
-             "github_repo": "akariv/gov-watch",
-             "leaders": [
-             "akariv",
-             "mushon"
-             ]
-             },*/
-            {
-                "name": "Anyway (שם זמני)",
-                "about": "נציג באופן בהיר ונגיש מידע על תאונות, כמעט-תאונות ומפגעי תנועה ונאפשר דיון פתוח וגלוי בין נוסע לנוסע ולמקבלי ההחלטות.",
-                "status": "דגירה",
-                "entry_level": "מתקדמים",
-                "github_repo": "https://github.com/hasadna/anyway",
-                "issuesUrl": "https://api.github.com/repos/hasadna/anyway/issues",
-                "slug": "anyway",
-                "leaders": [
-                    "sagism",
-                    "ronreiter"
-                ]
-            },
-            {
-                "name": "ספר החוקים",
-                "about": "מתוך ה'פרסומים הרשמיים' של חוק, לא ניתן לדעת את נוסחו. כאן נכנס לתמונה פרויקט החקיקה הישראלית, שנועד למלא את החסר באמצעות פרסום נוסחי החוק המעודכנים, על שינוייהם ורובדיהם.",
-                "status": "דגירה",
-                "entry_level": "מתחילים",
-                "issuesUrl": "https://api.github.com/repos/hasadna/openlaw-bot/issues",
-                "slug": "openlaw-bot",
-                "github_repo": "https://github.com/hasadna/openlaw-bot",
-                "leaders": [
-                    "uda"
-                ],
-                "permalink": "http://he.wikisource.org/wiki/%D7%A1%D7%A4%D7%A8_%D7%94%D7%97%D7%95%D7%A7%D…%D7%A9%D7%9C_%D7%9E%D7%93%D7%99%D7%A0%D7%AA_%D7%99%D7%A9%D7%A8%D7%90%D7%9C"
-            },
-            {
-                "name": "תביא ת'דאטה",
-                "about": "פלטפורמה פתוחה לקבלת מאגרי מידע ממשרדי הממשלה והרשויות השונות. הפלטפורמה הבסיסית הינה Alaveteli של הארגון הבריטי mySociety, אשר תורגמה והוסבה לשימוש מקומי.",
-                "status": "דגירה",
-                "entry_level": "מתקדמים",
-                "github_repo": "https://github.com/hasadna/alaveteli",
-                "issuesUrl": "https://api.github.com/repos/hasadna/alaveteli/issues",
-                "slug": "alaveteli",
-                "leaders": "yauzi",
-                "permalink": "http://askdata.org.il/"
-            },
-            {
-                "name": "תבע פתוחה",
-                "about": "הנגשת מסמכי התבע לאזרח הפשוט שאינו מצוי במונחי בינוי כמו מספרי גושים, חלקות וכן הלאה",
-                "permalink": "http://opentaba.info/",
-                "status": "רגע לפני בקיעה",
-                "entry_level": "מתקדמים",
-                "github_repo": "https://github.com/niryariv/opentaba-server",
-                "issuesUrl": "https://api.github.com/repos/hasadna/opentaba-server/issues",
-                "slug": "opentaba-server",
-                "leaders": "niryariv"
-            },
-            {
-                "name": "הגשמה ציבורית",
-                "about": "פלטפורמה מונחית פתרונות, לחיבור בין עובדי ציבור",
-                "status": "דגירה",
-                "github_repo": "https://github.com/TzahiM/NeuroNet",
-                "leaders": "TzahiM",
-                "slug": "kuterless",
-                "permalink": "http://kuterless.org.il/",
-                "entry_level": "מתחילים"
-            },
-            {
-                "name": "פנסיה פתוחה",
-                "about": "מאגר מידע שינגיש וישקף לציבור את האופן בו מושקעים כספי הפנסיה שלו",
-                "status": "לקראת בקיעה",
-                "issuesUrl": "https://api.github.com/repos/hasadna/OpenPension/issues",
-                "slug": "open-pension",
-                "github_repo": "https://github.com/hasadna/OpenPension",
-                "leaders": "Nevepere",
-                "entry_level": "מתקדמים"
-            }
-        ];
-// If we are running from node let be a module. 
-if (undefined !== module && module.exports)
-    module.exports.eKnightsData = eKnightsData;
+[
+  {
+    "name": "כנסת פתוחה",
+    "permalink": "http://oknesset.org",
+    "about": "החשמביר הראשון והאמא של הסדנא.  משרת פעילים וחברי כנסת למעלה משלוש שנים",
+    "status": "פרח מן הקן",
+    "entry_level": "מתקדמים",
+    "discussion_board": "https://groups.google.com/forum/#!forum/open-knesset",
+    "github_repo": "hasadna/Open-Knesset",
+    "leaders": ["ofri", "MeirKriheli", "amirnissim"],
+    "tags": ["Django", "Python", "Postgresql"]
+  },
+  {
+    "name": "קהילה פתוחה",
+    "permalink": "http://opencommunity.org.il",
+    "about": "הגברת רמת המעורבות של הציבור בקהילה על ידי שיתופו באופן שיטתי בתהליך קבלת ההחלטות, ומימוש, הלכה למעשה, את היותו הריבון של הקהילה.",
+    "status": "בקע מן הביצה",
+    "entry_level": "מתקדמים",
+    "discussion_board": "https://groups.google.com/forum/#!forum/omiflaga",
+    "github_repo": "hasadna/OpenCommunity",
+    "leaders": ["BoazChen", "nonZero"],
+    "tags": ["Django", "Python", "Bootstrap3"]
+  },
+  {
+    "name": "התקציב המקומי הפתוח",
+    "about": "עידוד רשויות מקומיות להתנהל לפי עקרונות של שקיפות ושיתוף הציבור והגברת מעורבותם של התושבים בהתנהלות הממשל המקומי",
+    "status": "דגירה",
+    "entry_level": "גיבורי-על",
+    "github_repo":"hasadna/openmuni-budgets",
+    "leaders": ["pwalsh","idoivri","ydaniv"],
+    "tags": ["Django", "Backbone", "D3", "jQuery"],
+	"discussion_board": "https://groups.google.com/forum/#!forum/open-municipality"
+  },
+    {
+    "name": "פותחן התקציב",
+    "permalink": "http://compare.open-budget.org.il",
+    "about": "השוואת תקציב המדינה לשנת 2013/2014 לתקציב השנים הקודמות והצפת הסעיפים שגדלו במיוחד או קטנו (בוצע בשיתןף עם ׳כלכליסט׳",
+    "status": "פרח מן הקן",
+    "entry_level": "גיבורי-על",
+    "github_repo":"OpenBudget/obudget_comparator",
+    "leaders": ["akariv","mushon"],
+    "tags": ["Backbone", "D3"]
+  },
+      {
+    "name": "התקציב הפתוח",
+    "permalink": "http://budget.msh.gov.il",
+    "about": "הצגת תקציב המדינה, מאז 1992 ועד 2014, השאאה היסטורית חיפוש ועוד",
+    "status": "פרח מן הקן",
+    "entry_level": "גיבורי-על",
+    "github_repo":"OpenBudget/obudget",
+    "leaders": ["akariv","daonb"],
+    "tags": ["GWT", "Django"]
+  },
+    {
+    "name": "מעקב טרכטנברג",
+    "permalink": "http://watch.gov.il",
+    "about": "מעקב אחר יישום המלצות וועדת טרכטנברג (הושק לרגל שנה למחאה החברתית, בשיתוף עם הממשלה וארגוני חברה אזרחית)",
+    "status": "פרח מן הקן",
+    "entry_level": "גיבורי-על",
+    "github_repo":"akariv/gov-watch",
+    "leaders": ["akariv","mushon"],
+    "tags": ["Flask", "Redis", "bootstrap", "isotope"]
+  },
+    {
+    "name": "Anyway (שם זמני)",
+    "about": "נציג באופן בהיר ונגיש מידע על תאונות, כמעט-תאונות ומפגעי תנועה ונאפשר דיון פתוח וגלוי בין נוסע לנוסע ולמקבלי ההחלטות.",
+    "status": "דגירה",
+    "entry_level": "מתקדמים",
+    "github_repo":"hasadna/anyway",
+    "leaders": ["sagism","ronreiter"],
+    "tags": ["Python","Django","Javascript"]
+  },
+    {
+    "name": "ספר החוקים",
+    "about": "מתוך ה'פרסומים הרשמיים' של חוק, לא ניתן לדעת את נוסחו. כאן נכנס לתמונה פרויקט החקיקה הישראלית, שנועד למלא את החסר באמצעות פרסום נוסחי החוק המעודכנים, על שינוייהם ורובדיהם.",
+    "status": "דגירה",
+    "entry_level": "מתחילים",
+    "github_repo":"hasadna/openlaw-bot",
+    "leaders": ["uda"],
+    "tags": ["Python","Django","Javascript"],
+	"permalink": "http://he.wikisource.org/wiki/%D7%A1%D7%A4%D7%A8_%D7%94%D7%97%D7%95%D7%A7%D7%99%D7%9D_%D7%A9%D7%9C_%D7%9E%D7%93%D7%99%D7%A0%D7%AA_%D7%99%D7%A9%D7%A8%D7%90%D7%9C"
+  },
+  {
+  "name": "רכבת פתוחה",
+  "about":"פתיחת והנגשת מדדי הדיוק של רכבת ישראל",
+  "status":"דגירה",
+  "entry_lecel":"מתחילים",
+  "github_repo":"hasadna/OpenTrain",
+  "leaders":["oferb","ekeydar"],
+  "tage":["Python","Android"]
+  }
+  ]
+ 
